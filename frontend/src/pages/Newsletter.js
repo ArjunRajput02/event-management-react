a015e8d45c8b1ba36c27ac6eea9e5a2b085a9f03
Arjun Rajput <arjun.rajput@seaflux.tech>
2026-02-16 18:26:09 +0530

    chore: remove unnecessary comment

--- frontend/src/pages/Newsletter.js
+++ frontend/src/pages/Newsletter.js
@@ -15,7 +15,6 @@ export async function action({ request }) {
   const data = await request.formData();
   const email = data.get('email');
 
-  // send to backend newsletter server ...
   console.log(email);
   return { message: 'Signup successful!' };
 }
