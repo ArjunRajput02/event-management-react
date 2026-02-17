9b7b079ab93bb1179bffa7f9309ed3efc8713fd1
Arjun Rajput <arjun.rajput@seaflux.tech>
2026-02-17 14:42:49 +0530

    chore: remove extra functions

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -3,20 +3,20 @@ import {
   useNavigate,
   useNavigation,
   useActionData,
-  redirect
-} from 'react-router-dom';
+  redirect,
+} from "react-router-dom";
 
-import classes from './EventForm.module.css';
+import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
   const data = useActionData();
   const navigate = useNavigate();
   const navigation = useNavigation();
 
-  const isSubmitting = navigation.state === 'submitting';
+  const isSubmitting = navigation.state === "submitting";
 
   function cancelHandler() {
-    navigate('..');
+    navigate("..");
   }
 
   return (
@@ -35,7 +35,7 @@ function EventForm({ method, event }) {
           type="text"
           name="title"
           required
-          defaultValue={event ? event.title : ''}
+          defaultValue={event ? event.title : ""}
         />
       </p>
       <p>
@@ -44,8 +44,7 @@ function EventForm({ method, event }) {
           id="image"
           type="url"
           name="image"
-          required
-          defaultValue={event ? event.image : ''}
+          defaultValue={event ? event.image : ""}
         />
       </p>
       <p>
@@ -55,7 +54,7 @@ function EventForm({ method, event }) {
           type="date"
           name="date"
           required
-          defaultValue={event ? event.date : ''}
+          defaultValue={event ? event.date : ""}
         />
       </p>
       <p>
@@ -65,7 +64,7 @@ function EventForm({ method, event }) {
           name="description"
           rows="5"
           required
-          defaultValue={event ? event.description : ''}
+          defaultValue={event ? event.description : ""}
         />
       </p>
       <div className={classes.actions}>
@@ -73,7 +72,7 @@ function EventForm({ method, event }) {
           Cancel
         </button>
         <button disabled={isSubmitting}>
-          {isSubmitting ? 'Submitting...' : 'Save'}
+          {isSubmitting ? "Submitting..." : "Save"}
         </button>
       </div>
     </Form>
@@ -87,23 +86,23 @@ export async function action({ request, params }) {
   const data = await request.formData();
 
   const eventData = {
-    title: data.get('title'),
-    image: data.get('image'),
-    date: data.get('date'),
-    description: data.get('description'),
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
   };
 
-  let url = 'http://localhost:8080/events';
+  let url = "http://localhost:8080/events";
 
-  if (method === 'PATCH') {
+  if (method === "PATCH") {
     const eventId = params.eventId;
-    url = 'http://localhost:8080/events/' + eventId;
+    url = "http://localhost:8080/events/" + eventId;
   }
 
   const response = await fetch(url, {
     method: method,
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(eventData),
   });
@@ -113,11 +112,10 @@ export async function action({ request, params }) {
   }
 
   if (!response.ok) {
-    throw new Response(JSON.stringify({ message: 'Could not save event.' }), {
+    throw new Response(JSON.stringify({ message: "Could not save event." }), {
       status: 500,
     });
   }
 
-  return redirect('/events');
+  return redirect("/events");
 }
-
